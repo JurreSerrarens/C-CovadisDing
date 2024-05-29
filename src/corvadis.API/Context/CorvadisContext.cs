9f571d031f3bb19b791302be793ad15c7dee60ae
JurreSerrarens <jurre.serrarens@student.graafschapcollege.nl>
2024-05-29 09:36:12 +0200

    Add rit

--- src/corvadis.API/Context/CorvadisContext.cs
+++ src/corvadis.API/Context/CorvadisContext.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using corvadis.API.models;
+using corvadis.API.Models;
 
 namespace corvadis.API.Context;
 
-public class GraafschapCollegeDbContext(DbContextOptions<GraafschapCollegeDbContext> options)
+public class CovadisDbContext(DbContextOptions<CovadisDbContext> options)
 : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
-    public DbSet<Car> Auto { get; set; }
-
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Ride> Rides { get; set; }
 }
---
+++ src/corvadis.API/Models/Ride.cs
@@ -0,0 +1,13 @@
+﻿namespace corvadis.API.Models
+{
+    public class Ride
+    {
+        public int Id { get; set; }
+
+        public double StartPosition { get; set; }
+        public double EndPosition { get; set; }
+        public string Destination { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
